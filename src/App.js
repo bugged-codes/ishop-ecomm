1774b2e7cb00abdb1d6ebd7b2b29b9ae30d70f7a
bugged-codes <vipul_49@outlook.com>
2023-07-27 14:48:16 +0530

    added render path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-
-
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import './App.css'
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import './App.css';
 import Header from './Component/Header';
 import Footer from './Component/Footer';
 
@@ -11,148 +9,129 @@ import Slider from './Component/Slider';
 import Login from './Component/Login';
 import SignUp from './Component/SignUp';
 import Routing from './Component/Routing';
-
+import axios from 'axios';
 
 export const CartContext = createContext();
-const API = "http://localhost:9000"
+const API = 'https://bugged-ishop.onrender.com';
 const App = () => {
-  const [mobiles, setMobiles] = useState([]);
-  useEffect(() => {
-    fetch(`${API}/mobiles`)
-      .then((data) => data.json())
-      .then((mobile) => setMobiles(mobile))
-  }, [])
-  const [cart, setCart] = useState([]);
-  useEffect(() => {
-    fetch(`${API}/cart`)
-      .then((data) => data.json())
-      .then((cartlist) => setCart(cartlist))
-  }, [])
+	const [mobiles, setMobiles] = useState([]);
+	useEffect(() => {
+		// fetch(`${API}/mobiles`)
+		//   .then((data) => data.json())
+		//   .then((mobile) => setMobiles(mobile))
 
-  const updateCart = ({ mobile, type }) => {
+		axios.get(`${API}/mobiles`).then(({ data }) => setMobiles(data));
+	}, []);
+	const [cart, setCart] = useState([]);
+	useEffect(() => {
+		// fetch(`${API}/cart`)
+		// 	.then((data) => data.json())
+		// 	.then((cartlist) => setCart(cartlist));
 
-    fetch(`${API}/cart?type=${type}`, {
-      method: 'PUT',
-      body: JSON.stringify(mobile),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then((data) => data.json())
-      .then((cartlist) => setCart(cartlist))
+		axios.get(`${API}/cart`).then(({ data }) => setCart(data));
+	}, []);
 
-  }
-  return (
-    <div>
-      <CartContext.Provider value={[cart, mobiles, updateCart]}>
-        <Header />
-        <Routing />
+	const updateCart = ({ mobile, type }) => {
+		fetch(`${API}/cart?type=${type}`, {
+			method: 'PUT',
+			body: JSON.stringify(mobile),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((data) => data.json())
+			.then((cartlist) => setCart(cartlist));
+	};
+	return (
+		<div>
+			<CartContext.Provider value={[cart, mobiles, updateCart]}>
+				<Header />
+				<Routing />
 
-        <Footer />
+				<Footer />
+			</CartContext.Provider>
+		</div>
+	);
+};
 
-      </CartContext.Provider>
-
-    </div>
-  )
-}
-
-export default App
-
+export default App;
 
 function PhoneList() {
-  const [, mobiles] = useContext(CartContext)
-  return (
-    <div className='phone-list-container'>
-      {
-        mobiles && mobiles.map((mobile) => (
-          <Phone mobile={mobile} key={mobile._id} />))
-      }
-    </div>
-  )
+	const [, mobiles] = useContext(CartContext);
+	return <div className="phone-list-container">{mobiles && mobiles.map((mobile) => <Phone mobile={mobile} key={mobile._id} />)}</div>;
 }
-
 
 function Phone({ mobile }) {
-  const [, , updateCart] = useContext(CartContext)
+	const [, , updateCart] = useContext(CartContext);
 
-
-  return (
-    <div className='phone-container'>
-      <img src={mobile.image} alt={mobile.model} className='phone-img' />
-      <h2 className='phone-name'>{mobile.name}</h2>
-      <p className='phone-company'>{mobile.catagory}</p>
-      <p className='old-price'>Rs.:{mobile.oPrice}/- </p>
-      <h2>Rs :{mobile.price} /-</h2>
-      <button className='add-btn'
-        onClick={() => updateCart({ mobile, type: "increment" })}>
-        <ShoppingCartIcon /> Add-to-Cart</button>
-    </div>
-  )
+	return (
+		<div className="phone-container">
+			<img src={mobile.image} alt={mobile.model} className="phone-img" />
+			<h2 className="phone-name">{mobile.name}</h2>
+			<p className="phone-company">{mobile.catagory}</p>
+			<p className="old-price">Rs.:{mobile.oPrice}/- </p>
+			<h2>Rs :{mobile.price} /-</h2>
+			<button className="add-btn" onClick={() => updateCart({ mobile, type: 'increment' })}>
+				<ShoppingCartIcon /> Add-to-Cart
+			</button>
+		</div>
+	);
 }
 
-
 function CartList() {
-  const [cart] = useContext(CartContext)
-  function sum() {
-    var total = 0;
-    for (var i = 0; i < cart.length; i++) {
-      total = total + parseInt(cart[i].price) * cart[i].qty;
-    }
-    return total;
-  }
+	const [cart] = useContext(CartContext);
+	function sum() {
+		var total = 0;
+		for (var i = 0; i < cart.length; i++) {
+			total = total + parseInt(cart[i].price) * cart[i].qty;
+		}
+		return total;
+	}
 
+	return (
+		<div className="cart-phone-list-container">
+			<h1 className="heading-cart">Cart-List</h1>
+			{cart.map((mobile) => (
+				<CartItem mobile={mobile} key={mobile._id} />
+			))}
 
-  return (
-    <div className='cart-phone-list-container'>
-      <h1 className='heading-cart'>Cart-List</h1>
-      {
-        cart.map((mobile) => (
-          <CartItem mobile={mobile} key={mobile._id} />
-        ))
-      }
+			<h2 className="total-amout">Total Order Amout : Rs. {sum()}/-</h2>
 
-
-      <h2 className='total-amout'>Total Order Amout : Rs. {sum()}/-</h2>
-
-      <div className='pay'>
-
-        <button className="button-pay" >Pay Now</button>
-      </div>
-    </div>
-  )
+			<div className="pay">
+				<button className="button-pay">Pay Now</button>
+			</div>
+		</div>
+	);
 }
 
 function CartItem({ mobile }) {
-  const [, , updateCart] = useContext(CartContext)
-  console.log(mobile.price)
-  return (
-    <div className='cart-container'>
-      <div className='cart-image-div'>
-        <img className='phone-img' src={mobile.image} alt={mobile.name} />
+	const [, , updateCart] = useContext(CartContext);
+	console.log(mobile.price);
+	return (
+		<div className="cart-container">
+			<div className="cart-image-div">
+				<img className="phone-img" src={mobile.image} alt={mobile.name} />
+			</div>
+			<div className="description-product">
+				<h2 className="phone-name">{mobile.name}</h2>
+				<p className="phone-company">{mobile.catagory}</p>
 
-      </div>
-      <div className='description-product'>
-        <h2 className='phone-name'>{mobile.name}</h2>
-        <p className='phone-company'>{mobile.catagory}</p>
-
-        <div className='cart-item-button'>
-          <button className='incre-decre-btn'
-            onClick={() => updateCart({ mobile, type: "increment" })}>+</button>
-          <span> Quantity : {mobile.qty} </span>
-          <button className='incre-decre-btn'
-            onClick={() => updateCart({ mobile, type: "decrement" })}>-</button>
-        </div>
-      </div>
-      <div>
-
-        <h2>Price : Rs. {mobile.price* mobile.qty} /-</h2>
-        <button className="button-buy" >Buy Now</button>
-
-      </div>
-
-
-    </div>
-  )
+				<div className="cart-item-button">
+					<button className="incre-decre-btn" onClick={() => updateCart({ mobile, type: 'increment' })}>
+						+
+					</button>
+					<span> Quantity : {mobile.qty} </span>
+					<button className="incre-decre-btn" onClick={() => updateCart({ mobile, type: 'decrement' })}>
+						-
+					</button>
+				</div>
+			</div>
+			<div>
+				<h2>Price : Rs. {mobile.price * mobile.qty} /-</h2>
+				<button className="button-buy">Buy Now</button>
+			</div>
+		</div>
+	);
 }
 
 export { CartList, PhoneList, Phone };
--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,84 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './Login.css';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
-const API = "http://localhost:9000"
+import { useNavigate } from 'react-router-dom';
+const API = 'https://bugged-ishop.onrender.com';
 
-const Login=()=>{
-  const navigate = useNavigate()
- 
+const Login = () => {
+	const navigate = useNavigate();
 
-  const [user, setUser]= useState({})
+	const [user, setUser] = useState({});
 
+	const onchangeee = (e) => {
+		setUser({ ...user, [e.target.name]: e.target.value });
+	};
 
-  const onchangeee= (e)=>{
-    setUser({...user,[e.target.name] : e.target.value})
-  }
+	const navigatetosignup = () => {
+		navigate('/signup');
+	};
 
-  const navigatetosignup = () =>{
-    navigate('/signup')
-  }
+	const signup = async (e) => {
+		e.preventDefault();
+		navigate('/');
 
-  const signup = async (e)=>{
-   
-    e.preventDefault();
-    navigate('/')
+		// await sUserdata( (prevObj)=>{
+		//   prevObj.push(user);
+		//   return (prevObj)
+		// })
+		// await userdata.push(user)
+		// console.log(userdata)
+		console.log(user);
+		axios
+			.post(`${API}/login`, {
+				body: user,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			.then(() => {
+				console.log('logged in');
+			});
+	};
 
-      // await sUserdata( (prevObj)=>{
-      //   prevObj.push(user);
-      //   return (prevObj)
-      // })
-      // await userdata.push(user)
-      // console.log(userdata)
-      console.log(user)
-      axios.post(`${API}/login`, {
-       
-      body: user,
-      headers: {
-        "Content-Type" : "application/json"
-      }
+	return (
+		<div className="app">
+			<div className="login-form">
+				<div className="title">Sign In</div>
+				<div className="form">
+					<form>
+						<div className="input-container">
+							<label>Username </label>
+							<input type="text" name="email" required onChange={onchangeee} />
+						</div>
+						<div className="input-container">
+							<label>Password </label>
+							<input type="password" name="password" required onChange={onchangeee} />
+						</div>
+						<div className="button-container">
+							<input type="submit" onClick={signup} />
+						</div>
+						<p className="forget-password">Forget Password ?</p>
+						<hr />
+						<div className="button-container">
+							<button className="button-create" onClick={navigatetosignup}>
+								Create New Account
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-      })
-      .then(()=>{console.log("logged in")})
-
-      
-
-    
-  }
-  
-
-	return(
-        <div className="app">
-      <div className="login-form">
-
-        <div className="title">Sign In</div>
-        <div className="form">
-     <form>
-       <div className="input-container">
-         <label>Username </label>
-         <input type="text" name="email" required onChange={onchangeee} />
-       </div>
-       <div className="input-container">
-         <label>Password </label>
-         <input type="password" name="password" required onChange={onchangeee} />
-       </div>
-       <div className="button-container">
-         <input type="submit" onClick={signup}/>
-       </div>
-       <p className='forget-password'>Forget Password ?</p>
-       <hr/>
-       <div className="button-container">
-         <button className="button-create" onClick={navigatetosignup}>Create New Account</button>
-       </div>
-
-     </form>
-   </div>
-
-
-      </div>
-    </div>
-		
-	)
-}
-
-export default Login    
+export default Login;
